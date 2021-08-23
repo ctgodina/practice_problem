07f5e0cd0d82e0e8a4f03789a722edf0891ac645
ctgodina <ctrejo@g.clemson.edu>
2021-08-23 12:08:57 -0400

    Update anthony_game.py
    Now it randomly samples without replacement 2 other types to upgrade to 7.1. Also prints the stats of each type

--- anthony_game.py
+++ anthony_game.py
@@ -10,7 +10,7 @@ class Unit:
         #Checks if the rank and unit types passed in are in the
         #list of ranks and unit types
         if(not(rank in ranks) or not(unit_type in unit_types)):
-            print("rank or unit type invalid")
+            print("rank ", rank, "or unit type ", unit_type, "invalid")
             quit()
         
         self.rank = rank
@@ -131,14 +131,15 @@ class Unit:
 print (unit_types)
 print(ranks)
 
-spaced_grouping = 2*3*2*3*3 #to get 7.1 I must space out by 6.1 I 6.1 X 6.1 X
-
-
 squad = []
 not_I = []
-for j in range(6): #6 groups dividing the 5 types equally to make up 2916 - 36 
+rand_types = random.sample(unit_types, 2)
+rand_type1 = rand_types[0]
+rand_type2 = rand_types[1]
+
+for k in range(96): #6 groups dividing the 5 types equally to make up 2916 - 36 
     for i in range(1,6):
-        not_I = not_I + [Unit(4, unit_types[i]) for j in range(96)]
+        not_I = not_I + [Unit(4, unit_types[i]) for j in range(6)]
 
 #7.2I = 7.1I 7.1X 7.1X
 #7.1I = 7I 7I 7I
@@ -157,9 +158,51 @@ for k in range(3):
     #6.1X 6.1X
     squad = squad + [not_I.pop(0) for x in range(2*108)]
 #7.1X 7.1X
-squad = squad + [not_I.pop(0) for x in range(1944)] #2*(2916/3) units make up 2 7.1X 
-
+#Below replacing I with a random unit type not I
+#7.1I = 7I 7I 7I
+for k in range(3):
+    #7I = 6.1I 6.1X 6.1X
+    #6.1I = 6I 6I
+    for j in range(2):
+        #6I = 5.1 I 5.1 X 5.1X    
+        #5.1I = 5I 5I
+        for i in range(2):
+            #5I = 4.1I 4.1X 4.1X
+            squad = squad + [Unit(4,rand_type1) for i in range(3)] #4.1I
+            for x in range(6):#4.1X 4.1X
+                not_rand_type = [item for item in not_I if( (item.unit_type is not rand_type1) or (item.unit_type is not rand_type2) )]
+                member = not_rand_type.pop(0)
+                not_I.remove(member)
+                squad = squad + [member]
+        #5.1X 5.1X
+        squad = squad + [not_I.pop(0) for x in range(2*2*9)] #9 units in 5I 2 of those in 5.1 and need 2 of 5.1X
+    #6.1X 6.1X
+    squad = squad + [not_I.pop(0) for x in range(2*108)]
+#Below replacing I with a random unit type not I
+#7.1I = 7I 7I 7I
+for k in range(3):
+    #7I = 6.1I 6.1X 6.1X
+    #6.1I = 6I 6I
+    for j in range(2):
+        #6I = 5.1 I 5.1 X 5.1X    
+        #5.1I = 5I 5I
+        for i in range(2):
+            #5I = 4.1I 4.1X 4.1X
+            squad = squad + [Unit(4,rand_type2) for i in range(3)] #4.1I
+            for x in range(6):#4.1X 4.1X
+                not_rand_type = [item for item in not_I if( (item.unit_type is not rand_type1) or (item.unit_type is not rand_type2) )]
+                member = not_rand_type.pop(0)
+                not_I.remove(member)
+                squad = squad + [member]
+        #5.1X 5.1X
+        squad = squad + [not_I.pop(0) for x in range(2*2*9)] #9 units in 5I 2 of those in 5.1 and need 2 of 5.1X
+    #6.1X 6.1X
+    squad = squad + [not_I.pop(0) for x in range(2*108)]
 print("SQUAD LENGTH OF ", len(squad))
+print("Random type number 1 is ", rand_type1)
+print("Random type number 2 is", rand_type2)
+for u_type in unit_types:
+    print("Amount of ", u_type, " in squad", len([member for member in squad if(member.unit_type is u_type)]))
 exit_loop = False
 while True:
     print(squad)
